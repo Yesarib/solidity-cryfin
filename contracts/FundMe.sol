c625782eec7fb58205234f417b3f3349eaedc372
Yesarib <yesaribaris23@gmail.com>
2025-01-16 15:33:53 +0300

    updates

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,16 +3,18 @@ pragma solidity 0.8.24;
 
 import {PriceConverter} from "../library/PriceConverter.sol";
 
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
-    uint256 public minUsd = 5e18;
+    uint256 public constant minUsd = 5e18;
 
     address[] public funders;
-    mapping (address => uint256) public addressToAmountFunded;
-    address public owner;
-    constructor(){
-        owner = msg.sender;
+    mapping(address => uint256) public addressToAmountFunded;
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
@@ -21,9 +23,12 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withDraw() public {
-        require(msg.sender == owner, "must be owner.");
-        for(uint256 funderIndex = 0; funderIndex<funders.length; funderIndex++){
+    function withDraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
@@ -31,7 +36,27 @@ contract FundMe {
         // reset array
         funders = new address[](0);
 
-        (bool callSuccess,) = payable (msg.sender).call{value: address(this).balance}("");
-        require(callSuccess,"Call failed");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+        revert();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner,"sender is not owner!");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    //
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
